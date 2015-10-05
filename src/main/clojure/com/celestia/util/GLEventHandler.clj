cf50684abd41ac621ec15b7b550e95d752b77088
John Ellis <john.ellis1392+c9@gmail.com>
2015-10-05 05:32:46 +0000

    Add Main class to clojure project

--- src/main/clojure/com/celestia/util/GLEventHandler.clj
+++ src/main/clojure/com/celestia/util/GLEventHandler.clj
@@ -1,30 +1,56 @@
 
+(import com.jogamp.opengl.GLEventListener)
+(import com.celestia.models)
+(ns com.celestia.util)
+
+(def mGameState (GameState.))
+
 ;; Event Handler class 
-(gen-class 
-    :name com.celestia.util.GLEventHandler
-    :implements com.jogamp.opengl.GLEventListener
+(defrecord GLEventHandler []
+    GLEventListener ; Implement Event Listener Interface
+    (defn init [glAutoDrawable] 
+        ())
     
-    (defn -init [] ())
+    (defn display [glAutoDrawable] 
+        (def mGameState (update))
+        (render mGameState))
     
-    (defn -display [glAutoDrawable] 
-        (let [[gl (.getGL glAutoDrawable)]]
-            (loop for gameObject in mGameState
-                  (do 
-                      (.update gameObject)))
+    (defn reshape [glAutoDrawable] 
+        ())
+    
+    (defn dispose [glAutoDrawable] 
+        ()))
+
+
+
+
+
+
+; (gen-class 
+;     :name com.celestia.util.GLEventHandler
+;     :implements com.jogamp.opengl.GLEventListener
+    
+;     (defn -init [] ())
+    
+;     (defn -display [glAutoDrawable] 
+;         (let [[gl (.getGL glAutoDrawable)]]
+;             (loop for gameObject in mGameState
+;                   (do 
+;                       (.update gameObject)))
                   
-            (loop for gameObject in mGameState
-                  (do 
-                      (.detectCollisions gameObject)))
+;             (loop for gameObject in mGameState
+;                   (do 
+;                       (.detectCollisions gameObject)))
                   
-            (loop for gameObject in mGameState 
-                  (do 
-                      (.render gameObject)))))
+;             (loop for gameObject in mGameState 
+;                   (do 
+;                       (.render gameObject)))))
     
-    (defn -dispose [glAutoDrawable] 
-        (let [[gl (.getGL glAutoDrawable)]]
-             ()))
+;     (defn -dispose [glAutoDrawable] 
+;         (let [[gl (.getGL glAutoDrawable)]]
+;              ()))
     
-    (defn -reshape [glAutoDrawable] 
-        (let [[gl (.getGL glAutoDrawable]] 
-              ()))))
+;     (defn -reshape [glAutoDrawable] 
+;         (let [[gl (.getGL glAutoDrawable]] 
+;               ()))))
 
