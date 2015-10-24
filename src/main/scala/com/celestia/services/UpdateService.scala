0c5e768877400560bae1eb236da27940816e4fe4
johnellis1392 <john.ellis1392@gmail.com>
2015-10-23 23:44:29 -0700

    Change scala UpdateService function

--- src/main/scala/com/celestia/services/UpdateService.scala
+++ src/main/scala/com/celestia/services/UpdateService.scala
@@ -5,8 +5,21 @@ import com.celestia.interfaces.{IGameWorld, IGameState, IUpdateService}
 import com.celestia.models.{Camera, GameWorld, GameState}
 import graphicslib3D.Matrix3D
 
+/**
+  * Service class for updating Game State
+  */
 class UpdateService extends IUpdateService {
   override def update(gameState:IGameState):IGameState={
+    new GameState(
+      update_camera(gameState.camera),
+      gameState.gameWorld.map i => {
+        case triangle:Triangle => {}
+        case sphere:Sphere => {}
+      }
+    )
+  }
+
+  /*override def update(gameState:IGameState):IGameState={
     new GameState(
       new GameWorld(
         List()
@@ -17,6 +30,6 @@ class UpdateService extends IUpdateService {
         gameState.camera.z
       )
     )
-  }
+  }*/
 }
 
